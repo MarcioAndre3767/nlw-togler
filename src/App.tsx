adcc918dc2747c5a7169347a153fb7440c65b87f
Marcio Andre Mendes <marcioandre.ti1@gmail.com>
2021-07-05 07:17:23 -0300

    finalizaçao e deploy do projeto no firebase

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { NewRoom } from './pages/NewRoom';
 import { Room } from './pages/Room';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 
 
 function App() {
@@ -14,7 +15,9 @@ function App() {
         <Switch>
           <Route path="/" exact={true} component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />        
+          <Route path="/rooms/:id" component={Room} /> 
+
+          <Route path="/admin/rooms/:id" component={AdminRoom}  />    
         </Switch>           
         </AuthContextProvider>  
     </BrowserRouter>
